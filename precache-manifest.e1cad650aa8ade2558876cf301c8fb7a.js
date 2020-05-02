7e68a501c24d688bf19d34fe38709fee764f8215
Mafry Rivera <mafryfernanda@gmail.com>
2020-05-02 12:52:41 -0700

    Updates

---
+++ precache-manifest.e1cad650aa8ade2558876cf301c8fb7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe186c2fded530d34f00e4925e98ce78",
+    "url": "/homework_19/index.html"
+  },
+  {
+    "revision": "3138379a4b246af610b8",
+    "url": "/homework_19/static/css/main.2bee089f.chunk.css"
+  },
+  {
+    "revision": "c73d60445793ef27b4cf",
+    "url": "/homework_19/static/js/2.a7affad6.chunk.js"
+  },
+  {
+    "revision": "6ffb35759cde484fe7f965b2d9bad155",
+    "url": "/homework_19/static/js/2.a7affad6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3138379a4b246af610b8",
+    "url": "/homework_19/static/js/main.76aa0257.chunk.js"
+  },
+  {
+    "revision": "73059e1a728d2d6ff31c",
+    "url": "/homework_19/static/js/runtime-main.4318b339.js"
+  }
+]);
